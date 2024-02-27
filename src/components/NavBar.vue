04ebd74dfe4043de8693b2cffcf7e8441b0d1159
omar-the-junior <omarmasoudonline@gmail.com>
2024-02-27 20:06:30 +0200

    Unit test the NavBar element

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,7 +13,9 @@ const moonIcon = computed(() =>
   <nav
     class="fixed left-0 top-0 z-50 flex w-screen justify-between bg-white px-5 py-6 shadow-md dark:bg-dark-blue lg:px-20"
   >
-    <RouterLink class="font-black md:text-xl lg:text-2xl" to="/">Where in the world</RouterLink>
+    <RouterLink class="font-black md:text-xl lg:text-2xl" to="/" role="link"
+      >Where in the world?</RouterLink
+    >
 
     <ActionButton class="text-sm md:text-lg" :icon="moonIcon" @click="themeStore.toggleTheme"
       >Dark Mode</ActionButton
--- src/components/__tests__/NavBar.spec.ts
+++ src/components/__tests__/NavBar.spec.ts
@@ -1,18 +1,37 @@
-import { mount } from '@vue/test-utils'
 import NavBar from '../NavBar.vue'
 import { createTestingPinia } from '@pinia/testing'
 import { useThemeStore } from '@/stores/theme'
+import { render, screen } from '@testing-library/vue'
+import { RouterLinkStub } from '@vue/test-utils'
 
-vi.mock('window')
+beforeEach(() => {
+  const pinia = createTestingPinia()
+  const themeStore = useThemeStore()
+
+  themeStore.currentTheme = 'dark'
+  render(NavBar, {
+    global: {
+      stubs: {
+        RouterLink: RouterLinkStub
+      },
+      plugins: [pinia]
+    }
+  })
+})
+
 describe('NavBar', () => {
   it('renders properly', () => {
-    const pinia = createTestingPinia()
-    const themeStore = useThemeStore()
+    // Assert that the NavBar component is rendered
+    expect(screen.getByRole('navigation')).toBeInTheDocument()
+  })
 
-    themeStore.currentTheme = 'dark'
-    const element = mount(NavBar, {
-      global: { plugins: [pinia] }
-    })
-    expect(element.text()).toContain(/Where in the world/i)
+  it('contains phrase "Where in the World?"', () => {
+    const phrase = screen.getByRole('link', { name: /Where in the World?/i })
+    expect(phrase).toBeInTheDocument()
+  })
+
+  it('contains dark mode button', () => {
+    const button = screen.getByRole('button', { name: /dark mode/i })
+    expect(button).toBeInTheDocument()
   })
 })
